d80b53a508a31cd6b2d405eff23ded83db710f96
Philip Levy <philipdlevy@gmail.com>
2022-01-08 06:57:24 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function destructivelyAppendCat(name){
+    cats.push("Ralph");
+  }
+
+  function destructivelyPrependCat(name){
+      cats.unshift("Bob");
+  }
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats, "Broom"];
+    return (newCats);
+}
+
+function prependCat(name) {
+    const newArray = ["Arnold", ...cats];
+    return (newArray);
+}
+
+function removeLastCat() {
+    const CopyOfCats = cats.slice(0, -1);
+    return CopyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice(1)
+    return copyOfCats;
+}
+
+
+/*
+function removeLastCat() {
+    copyOfCats = cats.slice(0, -1);
+    return (copyOfCats);
+}
+*/
