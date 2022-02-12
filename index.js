dc8523554c195626448f547927ab6f55b40f9463
Philip Levy <philipdlevy@gmail.com>
2022-02-12 11:09:33 -0700

    commit tree 109accd37b35ec41fb0ef29926fc0b5c65238a50

--- index.js
+++ index.js
@@ -1,51 +1,37 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
-beforeEach(function () {
-    cats.length = 0;
+function destructivelyAppendCat(name) {
+    cats.push(name)
+};
 
-    cats.push('Milo', 'Otis', 'Garfield');
-  });
-
-  function destructivelyAppendCat(name){
-    cats.push("Ralph");
-  }
-
-  function destructivelyPrependCat(name){
-      cats.unshift("Bob");
-  }
+function  destructivelyPrependCat(name) {
+    cats.unshift(name)
+};
 
 function destructivelyRemoveLastCat() {
-    cats.pop();
-}
+    cats.pop()
+};
 
 function destructivelyRemoveFirstCat() {
-    cats.shift();
-}
+    cats.shift()
+};
 
 function appendCat(name) {
-    const newCats = [...cats, "Broom"];
-    return (newCats);
+    const newCats = [...cats, name]
+    return newCats;
 }
 
-function prependCat(name) {
-    const newArray = ["Arnold", ...cats];
-    return (newArray);
+function  prependCat(name) {
+    const newCats = [name, ...cats]
+    return newCats;
 }
 
 function removeLastCat() {
-    const CopyOfCats = cats.slice(0, -1);
-    return CopyOfCats;
-}
+   const newCats = cats.slice(0, cats.length -1)
+   return newCats;
+}    
 
 function removeFirstCat() {
-    const copyOfCats = cats.slice(1)
-    return copyOfCats;
+    const newCats = cats.slice(1)
+    return newCats;
 }
-
-
-/*
-function removeLastCat() {
-    copyOfCats = cats.slice(0, -1);
-    return (copyOfCats);
-}
-*/
